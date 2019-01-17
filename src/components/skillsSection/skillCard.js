60f2645a55eadde169536dfd38528741f92aa6b1
SimasDei <simondeiman@gmail.com>
2019-01-17 23:43:19 +0200

    more styling.

--- src/components/skillsSection/skillCard.js
+++ src/components/skillsSection/skillCard.js
@@ -3,16 +3,14 @@ import Grid from '@material-ui/core/Grid';
 
 export default props => {
   return (
-    <Grid container>
-      <Grid item>
-        <div className="feature-box">
-          <i className={props.icon} />
-          <h3 style={{ color: props.color }} className="small-heading">
-            {props.skill}
-          </h3>
-          <p>Flip me!</p>
-        </div>
-      </Grid>
+    <Grid item lg={4} md={4} sm={6} xs={12} className="feature-box-container">
+      <div className="feature-box">
+        <i className={props.icon} />
+        <h3 style={{ color: props.color }} className="small-heading">
+          {props.skill}
+        </h3>
+        <p>Flip me!</p>
+      </div>
     </Grid>
   );
 };
--- src/components/skillsSection/skillsSection.js
+++ src/components/skillsSection/skillsSection.js
@@ -5,27 +5,16 @@ import SkillCard from './skillCard';
 export default () => {
   return (
     <section id="skills" className="skillsSection">
-      <Grid
-        container
-        justify="space-around"
-        className="skillsContainer"
-        spacing={16}
-      >
-        <Grid item lg="auto" md="auto">
-          <SkillCard icon="fab fa-html5 fa-5x" skill="HTML" color="orange" />
-        </Grid>
-        <Grid item lg="auto" md="auto">
-          <SkillCard icon="fab fa-css3 fa-5x" skill="CSS" color="blue" />
-        </Grid>
-        <Grid item lg="auto" md="auto">
-          <SkillCard icon="fab fa-js-square fa-5x" skill="JS" color="yellow" />
-        </Grid>
-        <Grid item lg="auto" md="auto">
-          <SkillCard icon="fab fa-react fa-5x" skill="REACT" color="blue" />
-        </Grid>
-        <Grid item lg="auto" md="auto">
-          <SkillCard icon="fab fa-node fa-5x" skill="NODE" color="green" />
-        </Grid>
+      <Grid container className="skillsContainer">
+        <SkillCard icon="fab fa-html5 fa-5x" skill="HTML" color="orange" />
+
+        <SkillCard icon="fab fa-css3 fa-5x" skill="CSS" color="blue" />
+
+        <SkillCard icon="fab fa-js-square fa-5x" skill="JS" color="yellow" />
+
+        <SkillCard icon="fab fa-react fa-5x" skill="REACT" color="blue" />
+
+        <SkillCard icon="fab fa-node fa-5x" skill="NODE" color="green" />
       </Grid>
     </section>
   );
