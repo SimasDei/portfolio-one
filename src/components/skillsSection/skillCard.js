8343bcb180bb7492f67a9a4dbf90bd495c52c7c2
SimasDei <simondeiman@gmail.com>
2019-01-18 13:56:27 +0200

    styling pie charts

--- src/components/skillsSection/skillCard.js
+++ src/components/skillsSection/skillCard.js
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import { PieChart } from 'react-easy-chart';
 
 export default props => {
+  let customStyle = props.customStyle;
   return (
     <Grid item lg={4} md={4} sm={6} xs={12} className="feature-box-container">
       <div className="feature-box">
@@ -10,13 +11,14 @@ export default props => {
         <h3 style={{ color: props.color }} className="small-heading">
           {props.skill}
         </h3>
-
+        <p className="donut-chart-percent">{props.percent}%</p>
         <PieChart
+          styles={{ '.pie-chart-slice': customStyle }}
           size={150}
-          innerHoleSize={100}
+          innerHoleSize={125}
           data={[
-            { key: 'A', value: 300, color: props.color },
-            { key: 'B', value: 60, color: 'white' }
+            { key: 'A', value: props.percent, color: props.color },
+            { key: 'B', value: 100 - props.percent, color: 'transparent' }
           ]}
         />
       </div>
--- src/components/skillsSection/skillsSection.js
+++ src/components/skillsSection/skillsSection.js
@@ -2,19 +2,52 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import SkillCard from './skillCard';
 
+const style = {
+  backgroundImage: 'linear-gradient( to left, #1d976c, #0cc3db)',
+  backgroundClip: 'text',
+  backgroundColor: 'orangered',
+  stroke: 'none'
+};
+
 export default () => {
   return (
     <section id="skills" className="skillsSection">
       <Grid container className="skillsContainer">
-        <SkillCard icon="fab fa-html5 fa-5x" skill="HTML" color="orange" />
+        <SkillCard
+          icon="fab fa-html5 fa-5x"
+          skill="HTML"
+          percent={Math.floor(Math.random() * 100) + 1}
+          customStyle={style}
+          color={style.backgroundColor}
+        />
 
-        <SkillCard icon="fab fa-css3 fa-5x" skill="CSS" color="blue" />
+        <SkillCard
+          icon="fab fa-css3 fa-5x"
+          skill="CSS"
+          customStyle={style}
+          percent={Math.floor(Math.random() * 100) + 1}
+        />
 
-        <SkillCard icon="fab fa-js-square fa-5x" skill="JS" color="yellow" />
+        <SkillCard
+          icon="fab fa-js-square fa-5x"
+          skill="JS"
+          color="yellow"
+          percent={Math.floor(Math.random() * 100) + 1}
+        />
 
-        <SkillCard icon="fab fa-react fa-5x" skill="REACT" color="skyblue" />
+        <SkillCard
+          icon="fab fa-react fa-5x"
+          skill="REACT"
+          color="skyblue"
+          percent={Math.floor(Math.random() * 100) + 1}
+        />
 
-        <SkillCard icon="fab fa-node fa-5x" skill="NODE" color="green" />
+        <SkillCard
+          icon="fab fa-node fa-5x"
+          skill="NODE"
+          color="green"
+          percent={Math.floor(Math.random() * 100) + 1}
+        />
       </Grid>
     </section>
   );
