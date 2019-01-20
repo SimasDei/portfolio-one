e2bd2e98ee92be6dfdcf840faef06d6515c8753c
SimasDei <simondeiman@gmail.com>
2019-01-20 16:41:57 +0200

    add flipable done projects, TODO: pass project info via props

--- src/components/portfolioSection/project.js
+++ src/components/portfolioSection/project.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
 
 const Project = () => {
   return (
-    <Grid className="project-item" item xs>
-      <div className="project-card">
-        <div className="card-side card-side-front">
-          <h3>Bookaroo</h3>
+    <Grid className="project-container" item xs>
+      <div className="project-item">
+        <div className="project-card">
+          <div className="card-side-front">
+            <Card className="front-card">
+              <CardMedia
+                image={require('../../assets/natours.PNG')}
+                title="Please go away"
+                style={{ height: '20rem' }}
+              />
+              <CardContent>
+                <h2>Ima card, Look at me !</h2>
+              </CardContent>
+            </Card>
+          </div>
+          <div className="card-side-back">
+            <h3>Back</h3>
+          </div>
         </div>
-        <div className="card-side card-side-back" />
       </div>
     </Grid>
   );
