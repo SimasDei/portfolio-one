673ab4908cddb3737b1f609fd2d05869b0b263c6
SimasDei <simondeiman@gmail.com>
2019-01-21 19:47:02 +0200

    add responsive image grid

--- src/components/statsSection/statsSection.js
+++ src/components/statsSection/statsSection.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
+// import Grid from '@material-ui/core/Grid';
 import Typed from 'react-typed';
 
 import Aux from '../../hoc/auxilary';
 
 const StatsSection = () => {
-  const course = [
+  const course1 = [
     {
       img: require('../../assets/courses/sass-course.PNG'),
       title: 'ADVANCED CSS AND SASS',
@@ -26,6 +26,79 @@ const StatsSection = () => {
       img: require('../../assets/webScreens/natours.PNG'),
       title: 'image',
       cols: 2
+    },
+    {
+      img: require('../../assets/webScreens/MERN-recipe-app.png'),
+      title: 'Image',
+      cols: 1
+    },
+    {
+      img: require('../../assets/webScreens/MERN-recipe-app.png'),
+      title: 'Image',
+      cols: 1
+    }
+  ];
+
+  const course2 = [
+    {
+      img: require('../../assets/courses/sass-course.PNG'),
+      title: 'ADVANCED CSS AND SASS',
+      url: 'https://www.udemy.com/advanced-css-and-sass/',
+      cols: 2
+    },
+    {
+      img: require('../../assets/webScreens/bookarooToo.png'),
+      title: 'Image',
+      cols: 1
+    },
+    {
+      img: require('../../assets/fullWebScreens/natours-fullScreen.png'),
+      title: 'Image',
+      cols: 1
+    },
+    {
+      img: require('../../assets/webScreens/MERN-recipe-app.png'),
+      title: 'Image',
+      cols: 1
+    },
+    {
+      img: require('../../assets/webScreens/MERN-recipe-app.png'),
+      title: 'Image',
+      cols: 1
+    },
+    {
+      img: require('../../assets/webScreens/MERN-recipe-app.png'),
+      title: 'Image',
+      cols: 1
+    }
+  ];
+
+  const course3 = [
+    {
+      img: require('../../assets/courses/sass-course.PNG'),
+      title: 'ADVANCED CSS AND SASS',
+      url: 'https://www.udemy.com/advanced-css-and-sass/',
+      cols: 2
+    },
+    {
+      img: require('../../assets/webScreens/bookarooToo.png'),
+      title: 'Image',
+      cols: 1
+    },
+    {
+      img: require('../../assets/webScreens/MERN-recipe-app.png'),
+      title: 'Image',
+      cols: 1
+    },
+    {
+      img: require('../../assets/webScreens/MERN-recipe-app.png'),
+      title: 'Image',
+      cols: 1
+    },
+    {
+      img: require('../../assets/webScreens/MERN-recipe-app.png'),
+      title: 'Image',
+      cols: 1
     }
   ];
 
@@ -35,8 +108,8 @@ const StatsSection = () => {
         <Typed
           className="stats-heading"
           strings={['Statistics and Accomplishments']}
-          typeSpeed={60}
-          backSpeed={70}
+          typeSpeed={50}
+          backSpeed={40}
           loop
         />
       </div>
@@ -50,12 +123,53 @@ const StatsSection = () => {
             Browser not supported :(
           </video>
         </div>
-        <Grid
-          className="stats-container"
-          container
-          justify="space-evenly"
-          alignItems="center"
-        />
+        <div className="stats-container">
+          <div className="stats-row">
+            <div className="stats-column">
+              {course1.map(item => (
+                <img
+                  key={Math.random(0, 1000)}
+                  src={item.img}
+                  alt={item.title}
+                  style={{ width: '100%' }}
+                />
+              ))}
+            </div>
+
+            <div className="stats-column">
+              {course2.map(item => (
+                <img
+                  key={Math.random(0, 1000)}
+                  src={item.img}
+                  alt={item.title}
+                  style={{ width: '100%' }}
+                />
+              ))}
+            </div>
+
+            <div className="stats-column">
+              {course3.map(item => (
+                <img
+                  key={Math.random(0, 1000)}
+                  src={item.img}
+                  alt={item.title}
+                  style={{ width: '100%' }}
+                />
+              ))}
+            </div>
+
+            <div className="stats-column">
+              {course1.map(item => (
+                <img
+                  key={Math.random(0, 1000)}
+                  src={item.img}
+                  alt={item.title}
+                  style={{ width: '100%' }}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
       </section>
     </Aux>
   );
