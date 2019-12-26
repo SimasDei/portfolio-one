5f76f9739c3b8080c538dc9d174030e1ac075cd4
Simas <simas@simas-dei.com>
2019-12-26 18:43:48 +0200

    refactor navigation styling

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,62 +1,54 @@
 import React from 'react';
 
+import './footer.scss';
+
 const footer = () => {
   return (
-    <footer className="footer" id="footer">
-      <h2 className="footer-heading">contact me</h2>
-      <div className="footer-container">
-        <ul className="footer-list">
-          <li className="footer-item">
-            <i className="footer-icon fab fa-linkedin fa-3x" />
+    <footer className='footer' id='footer'>
+      <h2 className='footer-heading'>contact me</h2>
+      <div className='footer-container'>
+        <ul className='footer-list'>
+          <li className='footer-item'>
+            <i className='footer-icon fab fa-linkedin fa-3x' />
             <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer-link"
-              href="https://linkedin.com/in/SimasDei"
+              target='_blank'
+              rel='noopener noreferrer'
+              className='footer-link'
+              href='https://linkedin.com/in/SimasDei'
             >
               linkedin
             </a>
-            <p className="footer-paragraph">linkedin.com/in/SimasDei</p>
+            <p className='footer-paragraph'>linkedin.com/in/SimasDei</p>
           </li>
-          <li className="footer-item">
-            <i className="footer-icon fab fa-github fa-3x" />
+          <li className='footer-item'>
+            <i className='footer-icon fab fa-github fa-3x' />
             <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer-link"
-              href="https://github.com/SimasDei"
+              target='_blank'
+              rel='noopener noreferrer'
+              className='footer-link'
+              href='https://github.com/SimasDei'
             >
               github
             </a>
-            <p className="footer-paragraph">github.com/SimasDei</p>
+            <p className='footer-paragraph'>github.com/SimasDei</p>
           </li>
-          <li className="footer-item">
-            <i className="footer-icon fas fa-envelope-open fa-3x" />
-            <a
-              rel="noopener noreferrer"
-              className="footer-link"
-              href="mailto:simas@simas-dei.com"
-            >
+          <li className='footer-item'>
+            <i className='footer-icon fas fa-envelope-open fa-3x' />
+            <a rel='noopener noreferrer' className='footer-link' href='mailto:simas@simas-dei.com'>
               email
             </a>
-            <p className="footer-paragraph">simas@simas-dei.com</p>
+            <p className='footer-paragraph'>simas@simas-dei.com</p>
           </li>
-          <li className="footer-item">
-            <i className="footer-icon fas fa-phone fa-3x" />
-            <a
-              rel="noopener noreferrer"
-              className="footer-link"
-              href="#contacts"
-            >
+          <li className='footer-item'>
+            <i className='footer-icon fas fa-phone fa-3x' />
+            <a rel='noopener noreferrer' className='footer-link' href='#contacts'>
               Phone
             </a>
-            <p className="footer-paragraph">+37066366218</p>
+            <p className='footer-paragraph'>+37066366218</p>
           </li>
         </ul>
       </div>
-      <h5 className="footer-copyright">
-        &copy; 2019 Simas-Dei.com. Made and hosted by: Simas Dei
-      </h5>
+      <h5 className='footer-copyright'>&copy; 2019 Simas-Dei.com. Made and hosted by: Simas Dei</h5>
     </footer>
   );
 };
--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './navigation.scss';
+
 const navigation = () => {
   let checked = () => {
     let checkbox = document.getElementById('navi-toggle');
@@ -10,51 +12,47 @@ const navigation = () => {
   };
 
   return (
-    <div className="navigation">
-      <input type="checkbox" className="navigation-checkbox" id="navi-toggle" />
-      <label className="navigation-button" htmlFor="navi-toggle">
-        <span className="navigation-icon"> </span>
+    <div className='navigation'>
+      <input type='checkbox' className='navigation-checkbox' id='navi-toggle' />
+      <label className='navigation-button' htmlFor='navi-toggle'>
+        <span className='navigation-icon'> </span>
       </label>
-      <div className="navigation-background"> </div>
+      <div className='navigation-background'> </div>
 
-      <div className="navigation-nav">
-        <ul className="navigation-list">
-          <li className="navigation-item">
-            <a onClick={checked} href="#top" className="navigation-link">
+      <div className='navigation-nav'>
+        <ul className='navigation-list'>
+          <li className='navigation-item'>
+            <a onClick={checked} href='#top' className='navigation-link'>
               top
             </a>
           </li>
-          <li className="navigation-item">
-            <a onClick={checked} href="#about" className="navigation-link">
+          <li className='navigation-item'>
+            <a onClick={checked} href='#about' className='navigation-link'>
               About
             </a>
           </li>
-          <li className="navigation-item">
-            <a onClick={checked} href="#skills" className="navigation-link">
+          <li className='navigation-item'>
+            <a onClick={checked} href='#skills' className='navigation-link'>
               skills
             </a>
           </li>
-          <li className="navigation-item">
-            <a onClick={checked} href="#stats" className="navigation-link">
+          <li className='navigation-item'>
+            <a onClick={checked} href='#stats' className='navigation-link'>
               courses
             </a>
           </li>
-          <li className="navigation-item">
-            <a onClick={checked} href="#numbers" className="navigation-link">
+          <li className='navigation-item'>
+            <a onClick={checked} href='#numbers' className='navigation-link'>
               stats
             </a>
           </li>
-          <li className="navigation-item">
-            <a
-              onClick={checked}
-              href="#certificates"
-              className="navigation-link"
-            >
+          <li className='navigation-item'>
+            <a onClick={checked} href='#certificates' className='navigation-link'>
               certificates
             </a>
           </li>
-          <li className="navigation-item">
-            <a onClick={checked} href="#footer" className="navigation-link">
+          <li className='navigation-item'>
+            <a onClick={checked} href='#footer' className='navigation-link'>
               contacts
             </a>
           </li>
