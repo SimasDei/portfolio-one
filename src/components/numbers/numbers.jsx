829c658abbb6130c7873385c491d5ade4f220fd1
Simas <simas@simas-dei.com>
2019-12-28 19:56:58 +0200

    separate number secsion styles

--- src/components/numbers/numbers.jsx
+++ src/components/numbers/numbers.jsx
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
 import items from './numbers.data';
+import './numbers.scss';
 
 const numbers = () => {
   return (
