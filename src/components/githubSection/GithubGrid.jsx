d57289b08019aef3b7c4fb7dd00efcccc3d6a6f7
Simas <simas@simas-dei.com>
2020-01-12 16:21:01 +0200

    add github component

---
+++ src/components/githubSection/GithubGrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GithubGrid = () => {
+  return (
+    <section className='git-grid'>
+      <div className='git-grid__container'></div>
+    </section>
+  );
+};
+
+export default GithubGrid;
