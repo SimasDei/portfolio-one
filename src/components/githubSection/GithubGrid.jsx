14d9a854c3e288fb271454481dccd5f35adadf63
Simas <simas@simas-dei.com>
2020-01-12 16:25:39 +0200

    add github grid image

--- src/components/githubSection/GithubGrid.jsx
+++ src/components/githubSection/GithubGrid.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import GridImage from '../../assets/images/github.png';
+
 const GithubGrid = () => {
   return (
     <section className='git-grid'>
-      <div className='git-grid__container'></div>
+      <div className='git-grid__container'>
+        <img className='git-grid__image' src={GridImage} alt='github commit grid' />
+      </div>
     </section>
   );
 };
