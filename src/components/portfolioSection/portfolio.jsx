97945f54acb5cfc757506218467fe3c0b74e8780
Simas <simas@simas-dei.com>
2020-02-18 18:57:13 +0200

    add main project handling

--- src/components/portfolioSection/portfolio.jsx
+++ src/components/portfolioSection/portfolio.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Typed from 'react-typed';
-import Project from './project';
 
 import { data } from './portfolio.data';
+import mylimiausia from '../../assets/projectGifs/mylimiausia.gif';
+import reflectus from '../../assets/projectGifs/reflectus--web.gif';
+import './MainProject.styles.css';
+
+import Project from './project';
+import MainProject from './MainProject';
 
 const renderProjects = () =>
   data.map(project => (
@@ -18,10 +24,30 @@ const renderProjects = () =>
     />
   ));
 
+const renderMainProjects = () => {
+  const projects = [
+    {
+      title: 'Mylimiausia',
+      tech: ['Vue', 'PHP', 'mySQL'],
+      image: mylimiausia,
+    },
+    {
+      title: 'Reflectus',
+      tech: ['Vue', 'PHP', 'mySQL'],
+      image: reflectus,
+    },
+  ];
+
+  return projects.map(project => <MainProject key={project.title} project={project} />);
+};
+
 const Portfolio = () => {
   return (
     <section id='portfolio' className='portfolio-section'>
       <Typed className='portfolio-heading' strings={['PORTFOLIO']} />
+      <Paper elevation={5} className='main-projects'>
+        {renderMainProjects()}
+      </Paper>
       <Grid
         container
         justify='center'
